ec59eaec75b0c4c899a624f2bfafe41715fe6b75
Maren Rindfleisch <maren.rindfleisch@gmail.com>
2022-12-22 13:44:06 +0100

    Added footer with Github URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      Weather
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/marenrindfleisch/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Maren Rindfleisch
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/MarenRindfleisch/react-weather-forecast"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
